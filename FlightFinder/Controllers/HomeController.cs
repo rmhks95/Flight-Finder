6b6c013d5799a829cfe9362ccee9643b14ab1681
rmhks95 <rmhks95@ksu.edu>
2017-11-28 16:29:02 -0600

    CsvReader Started and install more packages

---
+++ FlightFinder/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+using CsvHelper;
+
+namespace FlightFinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var airports = ReadFiles(@"airports.csv");
+            var flights = ReadFiles(@"flights.csv");
+            return View();
+        }
+
+        public IEnumerable<dynamic> ReadFiles(string filename)
+        {
+            var textReader = new StreamReader(filename);
+            var csv = new CsvReader(textReader);
+            var records = csv.GetRecords<dynamic>();
+
+            return records;
+        }
+
+    }
+
+    internal class airports
+    {
+    }
+
+    internal class flights
+    {
+
+    }
+}
---
+++ FlightFinder/Scripts/Tutorial.jsx
@@ -0,0 +1,13 @@
+﻿var CommentBox = React.createClass({  
+    render: function() {  
+        return (  
+          <div className="commentBox">  
+
+            </div>  
+      );  
+    }  
+});  
+ReactDOM.render(  
+  <CommentBox />,  
+  document.getElementById('content')  
+); 
