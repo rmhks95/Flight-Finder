64ba7b5d73bd96aefa661b45b4b2577c0de8bd2e
Ryan <rmhks95@ksu.edu>
2017-11-29 15:57:42 -0500

    Shows json with /json

--- FlightFinder/App_Start/RouteConfig.cs
+++ FlightFinder/App_Start/RouteConfig.cs
@@ -12,7 +12,13 @@ namespace FlightFinder
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
+
+            routes.MapRoute(
+                name: "Comments",
+                url: "json",
+                defaults: new { controller = "Home", action = "Comments" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- FlightFinder/Controllers/HomeController.cs
+++ FlightFinder/Controllers/HomeController.cs
@@ -6,11 +6,45 @@ using System.Reflection;
 using System.IO;
 using System.Web.Mvc;
 using CsvHelper;
+using FlightFinder.Models;
 
 namespace FlightFinder.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        [OutputCache(Location = System.Web.UI.OutputCacheLocation.None)]
+        public ActionResult Comments()
+        {
+            return Json(_comments, JsonRequestBehavior.AllowGet);
+        }
+
         // GET: Home
         public ActionResult Index()
         {
